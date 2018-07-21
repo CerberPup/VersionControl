9e6d2e12d159f222d74bd3c24b7e8273e271a2d7
Wojtek <wowr130013@gmail.com>
2018-07-21 18:56:41 +0200

    Ui changes

--- VCL/GeneratedFiles/ui_mainwindow.h
+++ VCL/GeneratedFiles/ui_mainwindow.h
@@ -12,11 +12,16 @@
 #include <QtCore/QVariant>
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QColumnView>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QListView>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QSplitter>
 #include <QtWidgets/QStatusBar>
-#include <QtWidgets/QToolBar>
+#include <QtWidgets/QTreeView>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -26,51 +31,127 @@ class Ui_MainWindow
 public:
     QAction *actionOpen;
     QAction *actionExit;
+    QAction *actionInitialize_Version_Control;
+    QAction *actionCommit_Changes;
     QWidget *centralWidget;
+    QGridLayout *gridLayout;
+    QSplitter *splitter_2;
+    QTreeView *treeView;
+    QSplitter *splitter;
+    QListView *listView;
+    QColumnView *columnView;
     QMenuBar *menuBar;
     QMenu *menuFile;
     QMenu *menuOption;
     QMenu *menuSettings;
-    QToolBar *mainToolBar;
+    QMenu *menuMenage;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(400, 300);
+        MainWindow->resize(782, 556);
         actionOpen = new QAction(MainWindow);
         actionOpen->setObjectName(QStringLiteral("actionOpen"));
         actionExit = new QAction(MainWindow);
         actionExit->setObjectName(QStringLiteral("actionExit"));
+        actionInitialize_Version_Control = new QAction(MainWindow);
+        actionInitialize_Version_Control->setObjectName(QStringLiteral("actionInitialize_Version_Control"));
+        actionCommit_Changes = new QAction(MainWindow);
+        actionCommit_Changes->setObjectName(QStringLiteral("actionCommit_Changes"));
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        gridLayout = new QGridLayout(centralWidget);
+        gridLayout->setSpacing(6);
+        gridLayout->setContentsMargins(11, 11, 11, 11);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        splitter_2 = new QSplitter(centralWidget);
+        splitter_2->setObjectName(QStringLiteral("splitter_2"));
+        QSizePolicy sizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(splitter_2->sizePolicy().hasHeightForWidth());
+        splitter_2->setSizePolicy(sizePolicy);
+        splitter_2->setOrientation(Qt::Horizontal);
+        splitter_2->setChildrenCollapsible(false);
+        treeView = new QTreeView(splitter_2);
+        treeView->setObjectName(QStringLiteral("treeView"));
+        treeView->setEnabled(true);
+        QSizePolicy sizePolicy1(QSizePolicy::Minimum, QSizePolicy::Expanding);
+        sizePolicy1.setHorizontalStretch(4);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(treeView->sizePolicy().hasHeightForWidth());
+        treeView->setSizePolicy(sizePolicy1);
+        treeView->setMinimumSize(QSize(100, 200));
+        treeView->setMaximumSize(QSize(16777215, 16777215));
+        treeView->setSizeIncrement(QSize(0, 0));
+        treeView->setBaseSize(QSize(0, 0));
+        splitter_2->addWidget(treeView);
+        treeView->header()->setVisible(true);
+        splitter = new QSplitter(splitter_2);
+        splitter->setObjectName(QStringLiteral("splitter"));
+        QSizePolicy sizePolicy2(QSizePolicy::MinimumExpanding, QSizePolicy::Expanding);
+        sizePolicy2.setHorizontalStretch(2);
+        sizePolicy2.setVerticalStretch(0);
+        sizePolicy2.setHeightForWidth(splitter->sizePolicy().hasHeightForWidth());
+        splitter->setSizePolicy(sizePolicy2);
+        splitter->setMinimumSize(QSize(200, 200));
+        splitter->setFrameShadow(QFrame::Plain);
+        splitter->setOrientation(Qt::Vertical);
+        splitter->setOpaqueResize(true);
+        splitter->setHandleWidth(5);
+        splitter->setChildrenCollapsible(false);
+        listView = new QListView(splitter);
+        listView->setObjectName(QStringLiteral("listView"));
+        QSizePolicy sizePolicy3(QSizePolicy::Expanding, QSizePolicy::Preferred);
+        sizePolicy3.setHorizontalStretch(0);
+        sizePolicy3.setVerticalStretch(0);
+        sizePolicy3.setHeightForWidth(listView->sizePolicy().hasHeightForWidth());
+        listView->setSizePolicy(sizePolicy3);
+        listView->setMinimumSize(QSize(0, 142));
+        splitter->addWidget(listView);
+        columnView = new QColumnView(splitter);
+        columnView->setObjectName(QStringLiteral("columnView"));
+        sizePolicy3.setHeightForWidth(columnView->sizePolicy().hasHeightForWidth());
+        columnView->setSizePolicy(sizePolicy3);
+        columnView->setMinimumSize(QSize(0, 100));
+        splitter->addWidget(columnView);
+        splitter_2->addWidget(splitter);
+
+        gridLayout->addWidget(splitter_2, 0, 0, 1, 1);
+
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 400, 21));
+        menuBar->setGeometry(QRect(0, 0, 782, 21));
         menuFile = new QMenu(menuBar);
         menuFile->setObjectName(QStringLiteral("menuFile"));
         menuOption = new QMenu(menuBar);
         menuOption->setObjectName(QStringLiteral("menuOption"));
         menuSettings = new QMenu(menuBar);
         menuSettings->setObjectName(QStringLiteral("menuSettings"));
+        menuMenage = new QMenu(menuBar);
+        menuMenage->setObjectName(QStringLiteral("menuMenage"));
+        menuMenage->setEnabled(true);
         MainWindow->setMenuBar(menuBar);
-        mainToolBar = new QToolBar(MainWindow);
-        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
         statusBar = new QStatusBar(MainWindow);
         statusBar->setObjectName(QStringLiteral("statusBar"));
         MainWindow->setStatusBar(statusBar);
+        QWidget::setTabOrder(treeView, listView);
 
         menuBar->addAction(menuFile->menuAction());
+        menuBar->addAction(menuMenage->menuAction());
         menuBar->addAction(menuSettings->menuAction());
         menuBar->addAction(menuOption->menuAction());
         menuFile->addAction(actionOpen);
         menuFile->addSeparator();
         menuFile->addAction(actionExit);
+        menuMenage->addAction(actionInitialize_Version_Control);
+        menuMenage->addAction(actionCommit_Changes);
 
         retranslateUi(MainWindow);
+        QObject::connect(actionExit, SIGNAL(triggered()), MainWindow, SLOT(close()));
 
         QMetaObject::connectSlotsByName(MainWindow);
     } // setupUi
@@ -78,11 +159,14 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
-        actionOpen->setText(QApplication::translate("MainWindow", "Open", nullptr));
+        actionOpen->setText(QApplication::translate("MainWindow", "Select Root Folder", nullptr));
         actionExit->setText(QApplication::translate("MainWindow", "Exit", nullptr));
+        actionInitialize_Version_Control->setText(QApplication::translate("MainWindow", "Initialize Version Control", nullptr));
+        actionCommit_Changes->setText(QApplication::translate("MainWindow", "Commit Changes", nullptr));
         menuFile->setTitle(QApplication::translate("MainWindow", "File", nullptr));
         menuOption->setTitle(QApplication::translate("MainWindow", "Help", nullptr));
         menuSettings->setTitle(QApplication::translate("MainWindow", "Settings", nullptr));
+        menuMenage->setTitle(QApplication::translate("MainWindow", "Menage", nullptr));
     } // retranslateUi
 
 };
